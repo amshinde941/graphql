49f6e759b929563fd3fae20fe79db47c875118f8
amshinde941 <amshinde941@gmail.com>
2021-08-11 21:48:07 +0530

    added server prisma

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-//set additional urql exchanges - current
-import { Provider, Client, defaultExchanges} from 'urql';
+import { Provider, Client, dedupExchange, fetchExchange} from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
+
+const cache = cacheExchange({})
 
 const client = new Client({
   url: 'http://localhost:4000',
-  exchanges: defaultExchanges
+  exchanges: [dedupExchange, cache, fetchExchange]
 })
 
 ReactDOM.render(
